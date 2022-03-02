fd81ff5fdadb167e53c491b82fd358a3cd8e279a
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-03-02 17:35:22 +0530

    Time: 4 ms (88.10%), Space: 7.7 MB (98.04%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
+        //The key thing to observe here is that if there is a number x and if its even , then the number of set bits(no if 1's) in it will be equal to number of set bits in x/2.
+//If number x is odd then then number of set bits in it will be equal to 1+no of set bits in x/2.
+//So we fill a 1d dp table by storing dp[0]=0 .
+//So for 1 , its odd , so 1/2=0 so for 1 its 1, and similarly we store for others using precomputed values.
+//This solution is O(n) time.
         vector<int>dp(n+1);
         dp[0]=0;
         for(int i=1;i<=n;i++)
