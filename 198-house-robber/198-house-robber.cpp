e55f7be1853cf89be26f3ff89ae2c1562bbb93a5
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-05-12 14:07:44 +0530

    Time: 5 ms (18.45%), Space: 7.7 MB (48.52%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //recursion+memoization=dp soln
    //  int rob(vector<int>& nums) {
    //      int n=nums.size();
    //      vector<int>dp(n,-1);
@@ -17,21 +18,45 @@ public:
    //     int notpik=0+helper(ind-1,nums,dp);
    //     return dp[ind]=max(pick,notpik);
    //  }
-     int rob(vector<int>& nums) {
+    //tabulated dp(iterative dp) soln
+    //TC->O(n), SC->O(n)
+    //  int rob(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>dp(n,-1);
+    //     return helper(n,nums,dp);
+    // }
+    // int helper(int ind,vector<int>&nums,vector<int>&dp){
+    //     dp[0]=nums[0];
+    //     for(int i=1;i<ind;i++)
+    //     {
+    //         int pick=nums[i];
+    //         if(i>1)
+    //             pick+=dp[i-2];                
+    //         int notpick=0+dp[i-1];
+    //         dp[i]=max(pick,notpick);
+    //     }
+    //     return dp[ind-1];
+    // }
+    
+    //space optimization soln
+    //TC->O(n),SC->O(1)
+    int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return helper(n,nums,dp);
+        return helper(n,nums);
     }
-    int helper(int ind,vector<int>&nums,vector<int>&dp){
-        dp[0]=nums[0];
+    int helper(int ind,vector<int>&nums){
+        int prev=nums[0];
+        int prev2=0;
         for(int i=1;i<ind;i++)
         {
             int pick=nums[i];
             if(i>1)
-                pick+=dp[i-2];                
-            int notpick=0+dp[i-1];
-            dp[i]=max(pick,notpick);
+                pick+=prev2;                
+            int notpick=0+prev;
+          int  curri=max(pick,notpick);
+            prev2=prev;
+            prev=curri;
         }
-        return dp[ind-1];
+        return prev;
     }
 };
