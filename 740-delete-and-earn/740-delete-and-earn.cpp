44feef7c287690abc2afa2bd7a1567c2830e3a02
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-05-12 16:16:22 +0530

    Time: 20 ms (48.27%), Space: 13.9 MB (42.83%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        //make a frequency table for nums[i]=0 to nums of end and now if u pick nums[i] then u cant pick nums[i-1] or nums[i+1] ie if from freq table u pick 4 u cant consider 3 and 1 .....make a dp table , dp state ->each dp[i] denoted max points u can earn by picking that index . and return the dp[last]
-        int maxe=*max_element(nums.begin(),nums.end());
-        int dp[20004];
-        vector<int>freq(20004);
+        vector<int>freq(10001);
         for(auto i:nums)
             freq[i]++;
-        dp[0]=0;
-        dp[1]=freq[1];
-        for(int i=2;i<=maxe;i++)
+        int include=0;
+        int exclude=0;
+        for(int i=0;i<=10000;i++)
         {
-            dp[i]=max(dp[i-1],dp[i-2]+freq[i]*i);
+            int newinc=freq[i]*i+exclude;
+            int newex=max(include,exclude);
+            include=newinc;
+            exclude=newex;
         }
-        return dp[maxe];
+        return max(include,exclude);
     }
 };
