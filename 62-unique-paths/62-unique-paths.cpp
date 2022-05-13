0c7c337a4615af3a23ed53fea08eafeca4dcce8e
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-05-13 23:48:02 +0530

    Time: 4 ms (24.75%), Space: 6.6 MB (26.44%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
     
+//     int helper(int m,int n,vector<vector<int> > &dp)
+//     {
+//         //base case
+//         //top down dp
+//         //step 1 ->indexes
+//         if(m==0 and n==0)
+//             return 1;
+//         if(m<0 or n<0)
+//             return 0;
+//         if(dp[m][n]!=-1)
+//             return dp[m][n];
+//         //step 2-> take all appraches
+//         int left=helper(m,n-1,dp);
+//         int up=helper(m-1,n,dp);
+//         return dp[m][n]=left+up;
+        
+//     }
     int helper(int m,int n,vector<vector<int> > &dp)
     {
-        //base case
-        //top down dp
-        //step 1 ->indexes
-        if(m==0 and n==0)
-            return 1;
-        if(m<0 or n<0)
-            return 0;
-        if(dp[m][n]!=-1)
-            return dp[m][n];
-        //step 2-> take all appraches
-        int left=helper(m,n-1,dp);
-        int up=helper(m-1,n,dp);
-        return dp[m][n]=left+up;
+        //bottom up iterative dp
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+               
+                if(i==0 and j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                
+                 int right=0,down=0;
+                if(i>0)
+                 down=dp[i-1][j];
+                if(j>0)
+                 right=dp[i][j-1];
+                dp[i][j]=down+right;
+            }
+        }
+        return dp[m-1][n-1];
+      
         
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,dp);
+        return helper(m,n,dp);
     }
 };
