a0d115bf6d2f3dceb84c52afe480a8d0b64eef8b
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-05-31 20:05:51 +0530

    Time: 5 ms (21.42%), Space: 7.9 MB (41.52%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        //similar to house robber problem except that here 1st and last element are adjacent , so we cannot select both of them together , so 1st we use same logic and calculate dp[n-1] by excluding 1st element and then do same by excluding last element , and the maximum of 2 is our answer.
         int n=nums.size();
+        vector<int>dp1(n+1,0);
+        vector<int>dp2(n+1,0);
         if(n==1)
             return nums[0];
-        int dpstart[n+2];
-        int dpend[n+2];
-        dpstart[0]=nums[0];
-        dpstart[1]=max(nums[0],nums[1]);
-        //including first element and excluding last element
+        dp1[0]=nums[0];
+        dp1[1]=max(nums[0],nums[1]);
         for(int i=2;i<n-1;i++)
         {
-            dpstart[i]=max(dpstart[i-1],dpstart[i-2]+nums[i]);
+            dp1[i]=max(dp1[i-1],nums[i]+dp1[i-2]);
         }
-        //including last element and excluding first element
         
-        dpend[0]=0;
-        dpend[1]=nums[1];
+        dp2[0]=0;
+        dp2[1]=nums[1];
         for(int i=2;i<n;i++)
         {
-            dpend[i]=max(dpend[i-1],dpend[i-2]+nums[i]);
+            dp2[i]=max(dp2[i-1],nums[i]+dp2[i-2]);
         }
-        return max(dpstart[n-2],dpend[n-1]);
+        return max(dp1[n-2],dp2[n-1]);
+
+        
     }
 };
