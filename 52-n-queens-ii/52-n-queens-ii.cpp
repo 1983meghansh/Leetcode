8fcd8f5f0ed237177659d2540e01077f5ca256f9
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-06-04 12:23:42 +0530

    Time: 4 ms (78.56%), Space: 6.6 MB (38.72%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+     vector<vector<string>>res;
+    
+    bool valid(vector<string>&board,int row,int col)
+    {
+        for(int i=row;i>=0;i--)   //checking cols
+            if(board[i][col]=='Q')
+                return false;
+        for(int i=row,j=col;i>=0 and j>=0;i--,j--)  //checking left diagonal
+            if(board[i][j]=='Q')
+                return false;
+        
+        for(int i=row,j=col;i>=0 and j<board.size();i--,j++) //check right diagonal
+            if(board[i][j]=='Q')
+                return false;
+        
+        return true;
+        
+    }
+    
+    void dfs(vector<string>&board,int row)
+    {
+        if(row==board.size())
+        {
+            res.push_back(board);
+            return;
+        }
+        
+        for(int i=0;i<board.size();i++)
+        {
+            if(valid(board,row,i))
+            {
+                board[row][i]='Q';
+                dfs(board,row+1);
+                board[row][i]='.';
+        }
+    }}
+    
+    
+    int totalNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        dfs(board,0);
+        return res.size();
+    }
+};
