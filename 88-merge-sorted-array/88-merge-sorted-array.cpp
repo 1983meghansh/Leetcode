341e2cf880334a773c2debc230a104abc0097f5e
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-06-07 16:02:02 +0530

    Time: 4 ms (49.07%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,11 +3,19 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         //dsa sheet , smart approach to start from back
         //take advantage of the fact that both are sorted and use 3 pointors
-        int first=m-1,sec=n-1,orig=m+n-1;
-        while(sec>=0)
+       int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 and j>=0)
         {
-            nums1[orig--]=first>=0 and nums1[first]>=nums2[sec]?nums1[first--]:nums2[sec--];
+            if(nums2[j]>=nums1[i])
+            {
+                nums1[k--]=nums2[j--];
+            }
+            else{
+                nums1[k--]=nums1[i--];
+            }
         }
+        while(j>=0)
+            nums1[k--]=nums2[j--];
         
       
         
