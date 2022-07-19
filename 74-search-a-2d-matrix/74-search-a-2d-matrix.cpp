8dc9e070de473c936b617fc6d5828fe353e5f9f8
1983meghansh <79457667+1983meghansh@users.noreply.github.com>
2022-07-19 23:56:38 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (99.94%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // #1 simple approach
+        // #1 simple approach , beautiful
         // acc to properties , elements increase if we go down and decrease if we go left
         //keep a pointer at last element of first row
         int i=0;
         int n=matrix.size();
         int m=matrix[0].size();
-        int j=m-1;
-        while(i<n and j>=0)
+        // int j=m-1;
+        // while(i<n and j>=0)
+        // {
+        //     if(matrix[i][j]==target)
+        //         return true;
+        //     if(matrix[i][j]>target)
+        //         j--;
+        //     else
+        //         i++;
+        // }
+        // return false;
+        
+        //However we can further optimize it 
+        //observe that if we place all elements in a 1d array , it will be sorted
+        //so we can apply binary search and get the target
+        //but we prefer not to use extra space
+        //so we give imaginary indexes to the matrix itself and perform bs and we can get the index by the formula index/m and index%m
+        int lo=0,hi=n*m-1;
+        while(lo<=hi)
         {
-            if(matrix[i][j]==target)
+            int mid=(lo+hi)/2;
+            if(matrix[mid/m][mid%m]==target)
                 return true;
-            if(matrix[i][j]>target)
-                j--;
+            if(matrix[mid/m][mid%m]>target)
+                hi=mid-1;
             else
-                i++;
+                lo=mid+1;
         }
         return false;
     }
